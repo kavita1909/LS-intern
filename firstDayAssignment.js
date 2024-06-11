00ed8b3f772f29cb9b7f8d6130897f9f96a1e1a7
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-11 17:00:39 +0530

    javascript pdfs

--- firstDayAssignment.js
+++ firstDayAssignment.js
@@ -1,4 +1,12 @@
-javascript can change HTML content. 
+PDF 1 : Introduction to Javascript
+Javascript is Object based language.
+lightweight programming language.
+cross platform programming language.
+client side scripting language.
+understanding of CSS and HTML. 
+Where JS is used. javascript can change HTML content. 
+
+PDF 2 : Javascript code examples
 javascript can change HTML attributes. 
 javascript can change the style of an HTML Element. 
 javascript can hide HTML elements. 
