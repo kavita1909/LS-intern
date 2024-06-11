dd7c32182f00accf0d442dad390090da89c40dc2
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-11 16:07:44 +0530

    introduction to Javascript

--- firstDayAssignment.js
+++ firstDayAssignment.js
@@ -1,1 +1,7 @@
 introduction to Javascript
+Javascript is Object based language.Javascript
+lightweight programming language.Javascript
+cross platform programming language.
+client side scripting language.Javascript
+understanding of CSS and HTML. 
+Where JS is used. 
