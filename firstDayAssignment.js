84251a024067c221da280d73b3021ecfe5993cf2
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-11 16:57:14 +0530

    javascript code examples

--- firstDayAssignment.js
+++ firstDayAssignment.js
@@ -1,7 +1,5 @@
-introduction to Javascript
-Javascript is Object based language.
-lightweight programming language.
-cross platform programming language.
-client side scripting language.
-understanding of CSS and HTML. 
-Where JS is used. 
+javascript can change HTML content. 
+javascript can change HTML attributes. 
+javascript can change the style of an HTML Element. 
+javascript can hide HTML elements. 
+javascript can show hidden HTML elements 
