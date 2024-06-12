4b5cb74c0a2b5e8081a7d750c18be575497ca675
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-12 13:09:01 +0530

    code example for Accessing object properties

--- code.js
+++ code.js
@@ -1,5 +1,12 @@
-var bike = { name: "HONDA", color: "blue", model: "500cc",
-    details:function() {
-return this.name + " " + this.color + " " + this.model;
-} };
-console.log(bike.details());
+// var mobname = "samsung";
+// myfunction();
+// function myfunction(){
+//     console.log("The type of mobile name is " + mobname);
+//      }
+
+var car = {model:"500", companyName:"honda", safety:600 }
+console.log(car);
+var key = "model"
+var p = "safety"
+var c = "companyName"
+console.log(car[key], car[p], car[c])
--- secondDayAssignment.js
+++ secondDayAssignment.js
@@ -4,6 +4,14 @@ Javascript Objects, methods and properties.
 Objects and variables and their differences.
 Object properties and methods
 Accessing object properties
+
+var car = {model:"500", companyName:"honda", safety:600 }
+console.log(car);
+var key = "model"
+var p = "safety"
+var c = "companyName"
+console.log(car[key], car[p], car[c])
+
 Accessing object methods:
 
 var bike = { name: "HONDA", color: "blue", model: "500cc",
@@ -14,3 +22,34 @@ console.log(bike.details());
 
 When a Javascript variable is declared with a keyword "new", the the variable is created as an object.
 ex : var x = new String();
+
+PDF 10 : Javascript Functions
+
+Javascript Functions:
+
+function sum(a, b) {
+    return a * b;
+}
+console.log(sum(4,3));
+
+why Functions?
+Javascript function syntax.
+function invocation, function return
+code hoe to use function :
+(console.log("the temperature is " + toC(77) + " celcius"));
+function toC(f) {
+    return (5/9) * (f-32);
+} 
+
+PDF 11 : Javascript scope
+
+local Javascript variables
+global Javascript variables
+
+var mobname = "samsung";
+myfunction();
+function myfunction(){
+    console.log("The type of mobile name is " + mobname);
+     }
+
+
