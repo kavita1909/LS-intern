48b7269716d92a557b2b3fe45fde24d79ee1f3f8
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-14 11:18:06 +0530

    one more program on NaN)

--- Day4/a.js
+++ Day4/a.js
@@ -1,6 +1,13 @@
 
+// let result =
+// "Is '123' NaN? " + isNaN('123') + "<br>" +
+// "Is 'Hello' NaN? " + isNaN('Hello') + "<br>" +
+// "Is '2005/12' NaN? " + isNaN('12+13');
+// console.log(result);
+
 let result =
-"Is '123' NaN? " + isNaN('123') + "<br>" +
-"Is 'Hello' NaN? " + isNaN('Hello') + "<br>" +
-"Is '2005/12' NaN? " + isNaN('12+13');
+"Is 123 NaN? " + isNaN(123) + "<br>" +
+"Is -1.23 NaN? " + isNaN(-1.23) + "<br>" +
+"Is 5-2 NaN? " + isNaN("5-2") + "<br>" +
+"Is 0 NaN? " + isNaN(0);
 console.log(result);
