399c44c4fde3436789fc41c618b8207f0e1258ee
kavita_kumari <chauhan.kavita2248@gmail.com>
2024-06-14 11:43:29 +0530

    program on toString()

--- Day4/a.js
+++ Day4/a.js
@@ -5,9 +5,12 @@
 // "Is '2005/12' NaN? " + isNaN('12+13');
 // console.log(result);
 
-let result =
-"Is 123 NaN? " + isNaN(123) + "<br>" +
-"Is -1.23 NaN? " + isNaN(-1.23) + "<br>" +
-"Is 5-2 NaN? " + isNaN("5-2") + "<br>" +
-"Is 0 NaN? " + isNaN(0);
-console.log(result);
+// let result =
+// "Is 123 NaN? " + isNaN(123) + "<br>" +
+// "Is -1.23 NaN? " + isNaN(-1.23) + "<br>" +
+// "Is 5-2 NaN? " + isNaN("5-2") + "<br>" +
+// "Is 0 NaN? " + isNaN(0);
+// console.log(result);
+
+let x = 123;
+console.log(x.toString(), " ", (12345).toString(), " ", (123+6899).toString());
